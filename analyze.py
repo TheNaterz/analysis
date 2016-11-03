e415470e15c6977c32fa44723d0aaf75d6b94d11
TheNaterz <nate.caroe@risksense.com>
2016-11-03 13:48:38 -0600

    sorting by keyspace

--- analyze.py
+++ analyze.py
@@ -1,4 +1,4 @@
-import sys, operator
+import sys
 
 patterns = {}
 
@@ -6,23 +6,29 @@ with open(sys.argv[1]) as f:
 	passwords = f.read().splitlines()
 
 for password in passwords:
+	value = []
 	pattern = ""
+	weight = 1
 	for char in password:
 		if char.islower():
 			pattern += "?l"
+			weight *= 26
 		elif char.isupper():
 			pattern += "?u"
+			weight *= 26
 		elif char.isdigit():
 			pattern += "?d"
+			weight *= 10
 		else:
 			pattern += "?s"
+			weight *= 33
 	try:
-		patterns[pattern] += 1
+		patterns[pattern][0] += 1
 	except KeyError:
-		patterns[pattern] = 1
+		patterns[pattern] = [1, weight]
 
 
-sorted_patterns = sorted(patterns.items(), key=operator.itemgetter(1), reverse=True)
+sorted_patterns = sorted(patterns.items(), key=lambda x:x[1][1])
 
 total = len(passwords)
 total *= 1.0
@@ -33,4 +39,4 @@ except IndexError:
 	limit = 19
 
 for i in range(0, int(limit)):
-	print("{} ... freq: {}").format(sorted_patterns[i], (sorted_patterns[i][1]/total)*100)
+	print("{} ... freq: {}").format(sorted_patterns[i], (sorted_patterns[i][1][0]/total)*100)
